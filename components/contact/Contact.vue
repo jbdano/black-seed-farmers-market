8b199c39bd8c8655f9ee9f534088e1c10b4e7feb
jdano <julian.dano2016@gmail.com>
2019-01-27 22:40:30 -0500

    updated success email notification UI

--- components/contact/Contact.vue
+++ components/contact/Contact.vue
@@ -92,15 +92,14 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Email Sent!</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">...</div>
+            <div class="modal-body">We will respond as soon as possible!</div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
