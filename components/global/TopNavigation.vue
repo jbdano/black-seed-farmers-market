8a2a72384eb309176112fd6b0bc674d9e74c4728
jdano <julian.dano2016@gmail.com>
2019-01-30 18:56:51 -0500

    fixed Top Navigation overlay bug on IOS safari

--- components/global/TopNavigation.vue
+++ components/global/TopNavigation.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="topnav" id="TopNavigation">
-    <nuxt-link to="/" class="tn-home-link tn-main-link"></nuxt-link>
-    <nuxt-link to="/Market" class="tn-market-link tn-main-link">Market</nuxt-link>
-    <nuxt-link to="/Delivery" class="tn-delivery-link tn-main-link">Delivery</nuxt-link>
-    <!-- <nuxt-link to="/membership" class="tn-membership-link tn-main-link">Membership</nuxt-link> -->
-    <nuxt-link to="/About" class="tn-about-link tn-main-link">About</nuxt-link>
-    <nuxt-link to="/Contact" class="tn-contact-link tn-main-link">Contact</nuxt-link>
-    <!-- <nuxt-link to="/membership" class="tn-become-member-link tn-main-link">Become a Member</nuxt-link> -->
-    <a href="javascript:void(0);" class="icon tn-main-link" v-on:click="openNav">
-      <i class="fa fa-bars"></i>
-    </a>
+  <div>
+    <div class="topnav" id="TopNavigation">
+      <nuxt-link to="/" class="tn-home-link tn-main-link"></nuxt-link>
+      <nuxt-link to="/Market" class="tn-market-link tn-main-link">Market</nuxt-link>
+      <nuxt-link to="/Delivery" class="tn-delivery-link tn-main-link">Delivery</nuxt-link>
+      <nuxt-link to="/About" class="tn-about-link tn-main-link">About</nuxt-link>
+      <nuxt-link to="/Contact" class="tn-contact-link tn-main-link">Contact</nuxt-link>
+      <a href="javascript:void(0);" class="icon tn-main-link" v-on:click="openNav">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
 
     <div id="mySidenav" class="sidenav">
       <a href="javascript:void(0)" class="closebtn" v-on:click="closeNav">&times;</a>
       <nuxt-link to="/" class="tn-market-link">Home</nuxt-link>
       <nuxt-link to="/Market" class="tn-market-link">Market</nuxt-link>
       <nuxt-link to="/Delivery" class="tn-delivery-link">Delivery</nuxt-link>
-      <!-- <nuxt-link to="/membership" class="tn-membership-link">Membership</nuxt-link> -->
       <nuxt-link to="/About" class="tn-about-link">About</nuxt-link>
       <nuxt-link to="/Contact" class="tn-contact-link">Contact</nuxt-link>
-      <!-- <nuxt-link to="/membership" class="tn-become-member-link">Become a Member</nuxt-link> -->
     </div>
   </div>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,11 @@ module.exports = {
     ]
   },
 
+  server: {
+    port: 8000, // default: 3000
+    host: "0.0.0.0" // default: localhost
+  },
+
   /*
    ** Customize the progress-bar color
    */
