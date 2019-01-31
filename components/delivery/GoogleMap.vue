0c0ee3551d1ffed310be1bd7aac07d600f9c97de
jdano <julian.dano2016@gmail.com>
2019-01-30 22:02:16 -0500

    adding fix for undefined variable in google maps

--- components/delivery/GoogleMap.vue
+++ components/delivery/GoogleMap.vue
@@ -13,35 +13,35 @@ export default {
     script: [
       {
         src:
-          "https://maps.googleapis.com/maps/api/js?key=AIzaSyBDI8NGakLgbE0pm8E7nz9YRSyt79XIEoQ",
-        async: false,
-        defer: false
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyBDI8NGakLgbE0pm8E7nz9YRSyt79XIEoQ"
       }
     ]
   },
   mounted() {
-    // The location of Uluru
-    var uluru = { lat: 42.262881, lng: -71.799387 };
-    var uluru2 = { lat: 42.2648, lng: -71.803999 };
+    if (process.browser) {
+      // The location of Uluru
+      var uluru = { lat: 42.262881, lng: -71.799387 };
+      var uluru2 = { lat: 42.2648, lng: -71.803999 };
 
-    var center_lat = (uluru.lat + uluru2.lat) / 2;
-    var center_lng = (uluru.lng + uluru2.lng) / 2;
-    // The map, centered at Uluru
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: { lat: center_lat, lng: center_lng }
-    });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      label: { text: "Mercantile Center", fontWeight: "bold" }
-    });
-    var marker2 = new google.maps.Marker({
-      position: uluru2,
-      map: map,
-      label: { text: "Fallon Health", fontWeight: "bold" }
-    });
+      var center_lat = (uluru.lat + uluru2.lat) / 2;
+      var center_lng = (uluru.lng + uluru2.lng) / 2;
+      // The map, centered at Uluru
+      var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: { lat: center_lat, lng: center_lng }
+      });
+      // The marker, positioned at Uluru
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        label: { text: "Mercantile Center", fontWeight: "bold" }
+      });
+      var marker2 = new google.maps.Marker({
+        position: uluru2,
+        map: map,
+        label: { text: "Fallon Health", fontWeight: "bold" }
+      });
+    }
   }
 };
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -43,15 +43,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-    { src: "~/plugins/LazyView.js", ssr: false }
-    // {
-    //   src:
-    //     "https://maps.googleapis.com/maps/api/js?key=AIzaSyBDI8NGakLgbE0pm8E7nz9YRSyt79XIEoQ",
-    //   async: true,
-    //   defer: true
-    // }
-  ],
+  plugins: [{ src: "~/plugins/LazyView.js", ssr: false }],
 
   /*
    ** Nuxt.js modules
