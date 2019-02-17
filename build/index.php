c8a60dfc256e5ebb531fb5b4d3cbde8a3410008b
JDano <JDano@DESKTOP-CV0VJQM>
2019-02-17 13:45:50 -0500

    major project restructure into PHP application

---
+++ build/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+
+<?php 
+$title = 'BSFM | Home';
+include 'head.php'; 
+?>
+</head>
+<body>
+
+<?php include 'navigation.php'; ?>
+
+
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
---
+++ build/navigation.php
@@ -0,0 +1,20 @@
+
+<div class="topnav" id="TopNavigation">
+	<a href="/" class="tn-home-link tn-main-link"></a> 
+	<a href="/Market" class="tn-market-link tn-main-link">Market</a> 
+	<a href="/Delivery" class="tn-delivery-link tn-main-link">Delivery</a> 
+	<a href="/About" class="tn-about-link tn-main-link">About</a> 
+	<a href="/Contact" class="tn-contact-link tn-main-link">Contact</a> 
+	<a href="javascript:void(0);" class="icon tn-main-link"> 
+		<i class="fa fa-bars"></i>
+	</a>
+</div>
+
+<div id="mySidenav" class="sidenav">
+	<a href="javascript:void(0)" class="closebtn">&times;</a> 
+	<a href="/" class="tn-market-link">Home</a> 
+	<a href="/Market" class="tn-market-link">Market</a> 
+	<a href="/Delivery" class="tn-delivery-link">Delivery</a> 
+	<a href="/About" class="tn-about-link">About</a> 
+	<a href="/Contact" class="tn-contact-link">Contact</a>
+</div>
